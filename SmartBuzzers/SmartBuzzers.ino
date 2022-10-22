0801fae7234372475ff7037cc7346a8244775fdf
Aaron Tacke <tacke@in.tum.de>
2022-10-22 15:38:39 +0200

    Implemented setLight method

--- SmartBuzzers/SmartBuzzers.ino
+++ SmartBuzzers/SmartBuzzers.ino
@@ -10,7 +10,17 @@ static const uint8_t lights[] = {2, 3, 4, 5, 6, 7, 8, 11};
 
 // Sets lights from 1 to 8, 0 means no light
 void setLight(uint8_t light){
-  // TODO
+  static uint8_t oldLight;
+  
+  // Turn off previous buzzer
+  if(oldLight > 0) pinMode(lights[oldLight-1], INPUT);
+  
+  // Turn on chosen buzzer
+  if(light > 0){
+    oldLight = light;
+    pinMode(lights[oldLight-1], OUTPUT);
+    digitalWrite(lights[oldLight-1], LOW);
+  }
 }
 
 
