3f29f7c62673c8457cad50bef908377897a9620b
Aaron Tacke <tacke@in.tum.de>
2022-10-22 16:54:47 +0200

    Implemented gamemode 'first'

--- SmartBuzzers/SmartBuzzers.ino
+++ SmartBuzzers/SmartBuzzers.ino
@@ -4,11 +4,11 @@
 
 
 // Pins for buttons of buzzers in correct order
-const uint8_t buttons[] = {A0, A1, A2, A3, A4, A5, A6, A7};
+const uint8_t buttons[] = {A2, A1, A6, A4, A7, A5, A3, A0};
 
 
 // Pins for lights of buzzers in correct order
-const uint8_t lights[] = {2, 3, 4, 5, 6, 7, 8, 11};
+const uint8_t lights[] = {11, 6, 3, 2, 4, 7, 8, 5};
 
 
 // Sets lights from 1 to 8, 0 means no light
@@ -34,6 +34,10 @@ uint8_t pressed(){
   }
   return 0;
 }
+
+
+const long timer[] = {86400000, 1000, 3000, 5000, 10000, 20000, 30000, 60000};
+uint8_t chosenTimer = 0;
 
 
 // Do setup if necessary
@@ -63,10 +67,50 @@ void loop() {
   }
 }
 
+bool delay_until(int millis_time, uint8_t player){
+  long endTime = millis() + millis_time;
+  while(millis()<endTime){
+    if(pressed() == player) return true;
+  }
+  return false;
+}
+
+void choose(uint8_t player){
+  // Wait until every button is released
+  while(pressed() != 0);
+  // Calculate end time in internal milliseconds
+  long endTime = millis() + timer[chosenTimer-1];
+  long timeLeft = endTime - millis();
+
+  // While time is left (pressing buzzer changes endTime)
+  while(timeLeft > 0){
+    // Blinking speed depending on time left
+    int light_time = min(sqrt(timeLeft)*5,666);
+    setLight(player);
+    // Change end time to now if player presses button
+    if(delay_until(light_time,player)) endTime = millis();
+    setLight(0);
+    // Change end time to now if player presses button
+    if(delay_until(light_time/2,player)) endTime = millis();
+    // Calculate remaining time left
+    timeLeft = endTime - millis();
+  }
+  // Wait until every button is released
+  while(pressed() != 0);
+}
+
 
 // Gamemode: The first player to press the button is chosen
 void gamemode_first(){
-  // TODO
+  // Wait until every button is released
+  while(pressed() != 0); 
+  // Choose time to answer
+  while(chosenTimer == 0) chosenTimer = pressed();
+  while(true){
+    uint8_t first = pressed();
+    while(first == 0) first = pressed();
+    choose(first);
+  }
 }
 
 
