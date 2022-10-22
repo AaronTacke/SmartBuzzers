ea4b04b7ff6865f423bd2c25ac652af0a9c20ba7
Aaron Tacke <tacke@in.tum.de>
2022-10-22 15:37:57 +0200

    Created Arduino Project

---
+++ SmartBuzzers/SmartBuzzers.ino
@@ -0,0 +1,23 @@
+
+
+// Pins for buttons of buzzers in correct order
+static const uint8_t buttons[] = {A0, A1, A2, A3, A4, A5, A6, A7};
+
+
+// Pins for lights of buzzers in correct order
+static const uint8_t lights[] = {2, 3, 4, 5, 6, 7, 8, 11};
+
+
+// Sets lights from 1 to 8, 0 means no light
+void setLight(uint8_t light){
+  // TODO
+}
+
+
+// Choose Gamemode and count/order players if necessary
+void setup() {
+  // TODO
+}
+
+// Replaced by specific loop functions for different Gamemodes
+void loop() {}
